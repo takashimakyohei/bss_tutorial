a33c9f3dfc01ca9dd78761b1240ec3fdd69da82e
takashimakyohei <hagi.suzu@icloud.com>
2020-03-10 20:49:27 +0900

    testcommit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>掲示板</h1>
+
+    <form action="post.php" method="post">
+
+        タイトル: <input type="text" placeholder="タイトルを入力してください" name="title">
+        <input type="submit" value="送信">
+    </form>
+
+
+</body>
+
+</html>
---
+++ post.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>投稿一覧</h1>
+    <!-- 投稿を取得して表示する -->
+    ：<?php
+    $title = $_POST["title"];
+
+    echo $title;
+    ?>
+
+    <p>
+        <a href="index.php">
+            <button>戻る</button>
+        </a>
+    </p>
+
+</body>
+
+</html>
