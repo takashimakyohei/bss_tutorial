949acd1fa107773f962a7ba0c68bb2c61e22721d
takashimakyohei <hagi.suzu@icloud.com>
2020-03-12 19:20:19 +0900

    ユーザー登録画面作成

---
+++ user-management.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<h1>ユーザー登録</h1>
+<form method="POST">
+<p>ユーザー名：<input type="text" name="user"></p>
+<p>メールアドレス：<input type="text" name="e-maiil"></p>
+<p>PW：<input type="text" name="pw"></p>
+<p>PW(再確認)：<input type="text" name="pw"></p>
+<button type="submit">登録</button>
+
+</form>
+</body>
+</html>
